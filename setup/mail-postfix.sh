2497bff4b3ad71cc30b410916ddcace10f0c8883
Jan Schulz-Hofen <jan@plan.io>
2017-11-25 00:04:59 +0100

    Disable Greylisting

--- setup/mail-postfix.sh
+++ setup/mail-postfix.sh
@@ -243,6 +243,12 @@ EOF
 chmod +x /etc/cron.daily/mailinabox-postgrey-whitelist
 /etc/cron.daily/mailinabox-postgrey-whitelist
 
+
+# effectively disable postgrey by adding /.*/ to the client whitelist
+cat > /etc/postgrey/whitelist_clients << EOF
+/.*/
+EOF
+
 # Increase the message size limit from 10MB to 128MB.
 # The same limit is specified in nginx.conf for mail submitted via webmail and Z-Push.
 tools/editconf.py /etc/postfix/main.cf \
